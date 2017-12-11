8e2b1a3e292f2a7570185d58a477ea1d573665f0
Francisco Pina Martins <f.pinamartins@gmail.com>
2017-12-11 17:33:58 +0000

    Better PEP8 conformance

--- tests/test_file_parser.py
+++ tests/test_file_parser.py
@@ -15,9 +15,13 @@
 # You should have received a copy of the GNU General Public License
 # along with pyRona.  If not, see <http://www.gnu.org/licenses/>.
 
-import pytest
+
 import pickle
 import pyRona.file_parser as fp
+
+import pytest
+assert pytest  # Hacky solution to stop the linters from complaining
+
 
 def test_parse_envfile():
     """
@@ -27,36 +31,48 @@ def test_parse_envfile():
     test_parsed = fp.parse_envfile("../tests/data/ENVFILE")
     test_parsed_list = [list(x) for x in test_parsed]
 
-    with open("../tests/data/jar/file_parser.parse_envfile.pickle","rb") as f:
-        control_parsed = pickle.load(f)
+    with open("../tests/data/jar/file_parser.parse_envfile.pickle",
+              "rb") as fle:
+        control_parsed = pickle.load(fle)
 
     control_parsed_list = [list(x) for x in control_parsed]
 
     assert test_parsed_list == control_parsed_list
 
-def test_baypass_summary_betai_parser():
+
+def test_baypass_s_b_p():
     """
     Test the function baypass_summary_betai_parser of file_parser.py.
     """
 
-    test_betai = fp.baypass_summary_betai_parser("../tests/data/Qsuber_GBS_mcmc_aux_summary_betai.out", 20, ["1", "2", "3"])
+    test_betai = fp.baypass_summary_betai_parser(
+        "../tests/data/Qsuber_GBS_mcmc_aux_summary_betai.out", 20,
+        ["1", "2", "3"])
 
-    with open("../tests/data/jar/file_parser.baypass_summary_betai_parser.pickle","rb") as f:
-        control_betai = pickle.load(f)
+    with open("../tests/data/jar/"
+              "file_parser.baypass_summary_betai_parser.pickle", "rb") as fle:
+        control_betai = pickle.load(fle)
 
     assert test_betai == control_betai
+
 
 def test_baypass_pij_parser():
     """
     Test the function baypass_pij_parser of file_parser.py.
     """
 
-    test_pij = fp.baypass_pij_parser("../tests/data/Qsuber_GBS_mcmc_aux_summary_pij.out", fp.baypass_summary_betai_parser("../tests/data/Qsuber_GBS_mcmc_aux_summary_betai.out", 20, ["1", "2", "3"]))
+    test_pij = fp.baypass_pij_parser(
+        "../tests/data/Qsuber_GBS_mcmc_aux_summary_pij.out",
+        fp.baypass_summary_betai_parser(
+            "../tests/data/Qsuber_GBS_mcmc_aux_summary_betai.out",
+            20, ["1", "2", "3"]))
 
-    with open("../tests/data/jar/file_parser.baypass_pij_parser.pickle","rb") as f:
-        control_pij = pickle.load(f)
+    with open("../tests/data/jar/"
+              "file_parser.baypass_pij_parser.pickle", "rb") as fle:
+        control_pij = pickle.load(fle)
 
     assert str(test_pij) == str(control_pij)
+
 
 def test_popnames_parser():
     """
@@ -65,7 +81,8 @@ def test_popnames_parser():
 
     test_popname = fp.popnames_parser("../tests/data/popnames_single_GEO.txt")
 
-    with open("../tests/data/jar/file_parser.popnames_parser.pickle","rb") as f:
-        control_popname = pickle.load(f)
+    with open("../tests/data/jar/"
+              "file_parser.popnames_parser.pickle", "rb") as fle:
+        control_popname = pickle.load(fle)
 
     assert test_popname == control_popname
