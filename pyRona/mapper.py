cf043aa53514ac079f3302ee1cece6826c8bdba5
Francisco Pina Martins <f.pinamartins@gmail.com>
2018-05-06 01:19:22 +0100

    Better understanding of the cartopy API. More plotting added

--- pyRona/mapper.py
+++ pyRona/mapper.py
@@ -15,6 +15,7 @@
 # along with pyRona.  If not, see <http://www.gnu.org/licenses/>.
 
 import cartopy.crs as ccrs
+import cartopy.feature as cfeature
 import matplotlib.pyplot as plt
 from file_parser import parse_lfmm_envfile
 import numpy as np
@@ -43,11 +44,22 @@ ax = plt.axes(projection=ccrs.Robinson())
 
 ax.set_extent([min_lat, max_lat, min_lon, max_lon])
 
+cfeature.BORDERS.scale = "50m"
+
 ax.coastlines(resolution='50m')
+ax.add_feature(cfeature.BORDERS)
 max_rona = 0.35
 cbat = np.arange(max_rona, 0, -0.01).reshape(int(max_rona * 100), 1)
 im = ax.imshow(cbat, cmap='gist_earth')
 
+# Plot dots
+ax.plot(8.8, 40.0, 'bo', markersize=22, transform=ccrs.PlateCarree())
+ax.plot(-8.4, 40.0, 'bo', markersize=22, transform=ccrs.Geodetic())
+
 plt.colorbar(im, ax=ax, label='RONA')
 
 fig.savefig("/tmp/aa.png")
+
+# TODO: Sample the dot colour from the colourbar
+# TODO: Eventually make an interpolation
+# TODO: Paint the sea
